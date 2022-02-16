9110acb3d3b9f5dfa0ec1343967acd092e2686e2
killu-k <killu.kaareste@gmail.com>
2022-02-16 09:59:43 +0200

    UserService

---
+++ src/main/java/valiit/game/result/domain/user/UserService.java
@@ -0,0 +1,12 @@
+package valiit.game.result.domain.user;
+
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+
+public class UserService {
+
+
+}
--- src/main/java/valiit/game/result/service/login/LoginController.java
+++ src/main/java/valiit/game/result/service/login/LoginController.java
@@ -1,10 +1,35 @@
 package valiit.game.result.service.login;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import valiit.game.result.domain.user.UserService;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/login")
+
 public class LoginController {
+    @Resource
+    UserService userService;
+
     // TODO: siin kasutate vaid LoginServicet.
     //  loginServices kasutate vaid UserServicet ja UserRoleServicet.
     //  eesmark on leida ylesse koigepealt user tabelist kasutaja
     //  seejarel leida user id jargi ylesse koik rollid.
     //  Ja mappida see info LoginResponse klassi ja tagastada see objekt
+
+    @GetMapping("/user") //vaata üle!!!!!!
+    public LoginResponse loginResponse (@RequestParam String userName, @RequestParam String password) {
+
+
+       return null;
+    }
+
+
+
+
 
 }
